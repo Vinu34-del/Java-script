0882c016cc3ebd3cabc764181dfa58cbaa306981
Vinu34-del <vd3711211@gmail.com>
2026-02-03 15:21:33 +0530

    modified

--- Variables and Data Types/first.js
+++ Variables and Data Types/first.js
@@ -1,1 +1,33 @@
-console.log("Hello Coders!");
+// VARIABLES AND DATA TYPES IN JAVASCRIPT
+
+Fullname = "Vinay Devadiga";
+age = 24;
+is_student = true;
+x = null;
+y = undefined;
+console.log(Fullname);
+
+// KEYWORDS IN JAVASCRIPT
+var city = "Bangalore"; // 'var' keyword (re-declarable and updatable)
+console.log(city)
+
+let country = "India"; // 'let' keyword (updatable but not re-declarable)
+console.log(country)
+
+const age = 24; // 'cannot be re-declared or updated. fixed value'
+
+
+// DATA TYPES IN JAVASCRIPT
+
+number = 100; // number data type
+string = "Hello World"; // string data type
+boolean = true; // boolean data type
+null_value = null; // null data type
+undefined_value = undefined; // undefined data type
+const student = {
+  fullname: "Vinay Devadiga",
+  cgpa: 8.9,
+  isgraduate: true,
+}; // object data type
+console.log(student);
+
