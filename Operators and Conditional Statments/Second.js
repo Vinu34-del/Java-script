cc2622654b46aa8e21ba1c78ebd1ed3cb48e82fa
Vinu34-del <vd3711211@gmail.com>
2026-02-04 22:19:33 +0530

    Operators

---
+++ Operators and Conditional Statments/Second.js
@@ -0,0 +1,42 @@
+// // Operators
+
+// // Airthemetic Operators
+// let a = 20;
+// let b = 10;
+// console.log("a + b =",a+b); // Addition
+// console.log("a - b =",a-b); // Subtraction
+// console.log("a * b =",a*b); // Multiplication
+// console.log("a / b =",a/b); // Division
+// console.log("a % b =",a%b); // Modulus
+// console.log("a ** b =",a**b); // Exponentiation
+
+// //Unary Operators
+// let c = 5;
+// c++;
+// console.log("c =",c);
+
+// // Assignment Operators
+
+// let a = 5;
+// let b = 2;
+// a -= 3;
+// a += 3;
+// a *= 3;
+
+// Comparison Operators
+// let a = 5;
+// let b = 3;
+// console.log("a == b", a == b); 
+// console.log("a != b", a != b); 
+// console.log("a == b", a == b); 
+// console.log("a > b", a > b);
+// console.log("a < b", a < b);
+
+// Logical Operators
+let a = 5;
+let b = 2;
+
+let con1 = a > b;
+let con2 = a === 5;
+console.log( con1 && con2)
+console.log( con1 || con2)
