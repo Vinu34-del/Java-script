74581c1300fe716986209c5bad4fb945cc2eda29
Vinu34-del <vd3711211@gmail.com>
2026-02-06 10:23:44 +0530

    Conditional statment

--- Operators and Conditional Statments/Second.js
+++ Operators and Conditional Statments/Second.js
@@ -80,3 +80,28 @@ let age = 20;
 
 let output = age >= 18 ? "Adult" : "minor";
 console.log("you are:", output);
+
+// Practise questions
+let num = prompt("Enter a number");
+
+if (num % 5 === 0) {
+  console.log(num, "is divide by 5");
+} else {
+  console.log(num, "is not divide by 5");
+}
+
+//Practise questions 2
+let score = prompt("Enter your score");
+let grade;
+if (score >= 90 && score <= 100) {
+  grade = "A";
+} else if (score >= 70 && score <= 89) {
+  grade = "B";
+} else if (score >= 60 && score <= 69) {
+  grade = "C";
+} else if (score >= 50 && score <= 59) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+console.log("Your grade is", grade);
