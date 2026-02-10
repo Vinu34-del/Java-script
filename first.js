6baee03315fcfc1e01bc7fa32daa517543131c24
Vinu34-del <vd3711211@gmail.com>
2026-02-10 22:44:35 +0530

    Loops and strings

---
+++ first.js
@@ -0,0 +1,57 @@
+// //For loops
+// for ( let count = 1; count <= 5; count++){
+//     console.log("Heyy Vinay..!", count);
+// }
+// console.log("Loop Ended");
+
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//   console.log("Heyy Vinay..!", i);
+//   sum = sum + i;
+// }
+// console.log("Total sum is :", sum);
+// console.log("Loop Ended");
+
+//While Loops
+// let i = 1;
+// while (i <= 5){
+//     console.log("Hello Coders..!", i);
+//     i++;
+// }
+
+// do-while loop
+// let i = 20;
+// do{
+//     console.log("Vinay");
+//     i++;
+// } while(i <= 10);
+
+//for of loop
+// let str = "Vinay";
+
+// for( let n of str){
+//     console.log(n)
+// }
+
+//practise question
+
+// for (let num =0; num<=100; num++){
+//     if(num % 2 == 0){
+//         console.log("Num is=",num);
+//     }
+//     console.log("Loop was ended");
+// }
+
+//practise question
+// let gameNum = 25;
+// let userNum = prompt("Enter the number");
+// while (userNum != gameNum) {
+//   userNum = prompt("you entered wrong number, try again:");
+// }
+// console.log("Congratulation, your guess is correct");
+
+//Strings
+let str = "Vinay";
+console.log(str.length);
+let str2 = 'Ramesh';
+console.log(str2.length);
